3627ec3b0ce2a33b40e3760b9381ececc49f2656
andre1xyz <andreu.pc.1997@gmail.com>
2025-02-05 19:24:13 +0000

    png fixes

--- invitations.js
+++ invitations.js
@@ -4,251 +4,251 @@ const INVITATIONS = {
         back: 'invitations/xaviermarc_back.PNG'
     },
     'andreumontse': {
-        front: 'invitations/andreumontse_front.PNG',
-        back: 'invitations/andreumontse_back.PNG'
+        front: 'invitations/andreumontse_front.png',
+        back: 'invitations/andreumontse_back.png'
     },
     'aaron': {
-        front: 'invitations/aaron_front.PNG',
-        back: 'invitations/aaron_back.PNG'
+        front: 'invitations/aaron_front.png',
+        back: 'invitations/aaron_back.png'
     },
     'adrifabianeric': {
-        front: 'invitations/adrifabianeric_front.PNG',
-        back: 'invitations/adrifabianeric_back.PNG'
+        front: 'invitations/adrifabianeric_front.png',
+        back: 'invitations/adrifabianeric_back.png'
     },
     'agustiteresa': {
-        front: 'invitations/agustiteresa_front.PNG',
-        back: 'invitations/agustiteresa_back.PNG'
+        front: 'invitations/agustiteresa_front.png',
+        back: 'invitations/agustiteresa_back.png'
     },
     'albert': {
-        front: 'invitations/albert_front.PNG',
-        back: 'invitations/albert_back.PNG'
+        front: 'invitations/albert_front.png',
+        back: 'invitations/albert_back.png'
     },
     'aina': {
-        front: 'invitations/aina_front.PNG',
-        back: 'invitations/aina_back.PNG'
+        front: 'invitations/aina_front.png',
+        back: 'invitations/aina_back.png'
     },
     'albertmaria': {
-        front: 'invitations/albertmaria_front.PNG',
-        back: 'invitations/albertmaria_back.PNG'
+        front: 'invitations/albertmaria_front.png',
+        back: 'invitations/albertmaria_back.png'
     },
     'aleixsian': {
-        front: 'invitations/aleixsian_front.PNG',
-        back: 'invitations/aleixsian_back.PNG'
+        front: 'invitations/aleixsian_front.png',
+        back: 'invitations/aleixsian_back.png'
     },
     'alexgermanrio': {
-        front: 'invitations/alexgermanrio_front.PNG',
-        back: 'invitations/alexgermanrio_back.PNG'
+        front: 'invitations/alexgermanrio_front.png',
+        back: 'invitations/alexgermanrio_back.png'
     },
     'annacristianleia': {
-        front: 'invitations/annacristianleia_front.PNG',
-        back: 'invitations/annacristianleia_back.PNG'
+        front: 'invitations/annacristianleia_front.png',
+        back: 'invitations/annacristianleia_back.png'
     },
     'amparoarshad': {
-        front: 'invitations/amparoarshad_front.PNG',
-        back: 'invitations/amparoarshad_back.PNG'
+        front: 'invitations/amparoarshad_front.png',
+        back: 'invitations/amparoarshad_back.png'
     },
     'ariadna': {
-        front: 'invitations/ariadna_front.PNG',
-        back: 'invitations/ariadna_back.PNG'
+        front: 'invitations/ariadna_front.png',
+        back: 'invitations/ariadna_back.png'
     },
     'cinta': {
-        front: 'invitations/cinta_front.PNG',
-        back: 'invitations/cinta_back.PNG'
+        front: 'invitations/cinta_front.png',
+        back: 'invitations/cinta_back.png'
     },
     'claudiacris': {
-        front: 'invitations/claudiacris_front.PNG',
-        back: 'invitations/claudiacris_back.PNG'
+        front: 'invitations/claudiacris_front.png',
+        back: 'invitations/claudiacris_back.png'
     },
     'cristinajan': {
-        front: 'invitations/cristinajan_front.PNG',
-        back: 'invitations/cristinajan_back.PNG'
+        front: 'invitations/cristinajan_front.png',
+        back: 'invitations/cristinajan_back.png'
     },
     'daniel': {
-        front: 'invitations/daniel_front.PNG',
-        back: 'invitations/daniel_back.PNG'
+        front: 'invitations/daniel_front.png',
+        back: 'invitations/daniel_back.png'
     },
     'danielines': {
-        front: 'invitations/danielines_front.PNG',
-        back: 'invitations/danielines_back.PNG'
+        front: 'invitations/danielines_front.png',
+        back: 'invitations/danielines_back.png'
     },
     'davidelviraonapol': {
-        front: 'invitations/davidelviraonapol_front.PNG',
-        back: 'invitations/davidelviraonapol_back.PNG'
+        front: 'invitations/davidelviraonapol_front.png',
+        back: 'invitations/davidelviraonapol_back.png'
     },
     'davidmontsejan': {
-        front: 'invitations/davidmontsejan_front.PNG',
-        back: 'invitations/davidmontsejan_back.PNG'
+        front: 'invitations/davidmontsejan_front.png',
+        back: 'invitations/davidmontsejan_back.png'
     },
     'eduangi': {
-        front: 'invitations/eduangi_front.PNG',
-        back: 'invitations/eduangi_back.PNG'
+        front: 'invitations/eduangi_front.png',
+        back: 'invitations/eduangi_back.png'
     },
     'estevemontse': {
-        front: 'invitations/estevemontse_front.PNG',
-        back: 'invitations/estevemontse_back.PNG'
+        front: 'invitations/estevemontse_front.png',
+        back: 'invitations/estevemontse_back.png'
     },
     'fernandoanna': {
-        front: 'invitations/fernandoanna_front.PNG',
-        back: 'invitations/fernandoanna_back.PNG'
+        front: 'invitations/fernandoanna_front.png',
+        back: 'invitations/fernandoanna_back.png'
     },
     'gerard': {
-        front: 'invitations/gerard_front.PNG',
-        back: 'invitations/gerard_back.PNG'
+        front: 'invitations/gerard_front.png',
+        back: 'invitations/gerard_back.png'
     },
     'gerardberta': {
-        front: 'invitations/gerardberta_front.PNG',
-        back: 'invitations/gerardberta_back.PNG'
+        front: 'invitations/gerardberta_front.png',
+        back: 'invitations/gerardberta_back.png'
     },
     'gerardlauraliam': {
-        front: 'invitations/gerardlauraliam_front.PNG',
-        back: 'invitations/gerardlauraliam_back.PNG'
+        front: 'invitations/gerardlauraliam_front.png',
+        back: 'invitations/gerardlauraliam_back.png'
     },
     'harsha': {
-        front: 'invitations/harsha_front.PNG',
-        back: 'invitations/harsha_back.PNG'
+        front: 'invitations/harsha_front.png',
+        back: 'invitations/harsha_back.png'
     },
     'helena': {
-        front: 'invitations/helena_front.PNG',
-        back: 'invitations/helena_back.PNG'
+        front: 'invitations/helena_front.png',
+        back: 'invitations/helena_back.png'
     },
     'joancamila': {
-        front: 'invitations/joancamila_front.PNG',
-        back: 'invitations/joancamila_back.PNG'
+        front: 'invitations/joancamila_front.png',
+        back: 'invitations/joancamila_back.png'
     },
     'joancinta': {
-        front: 'invitations/joancinta_front.PNG',
-        back: 'invitations/joancinta_back.PNG'
+        front: 'invitations/joancinta_front.png',
+        back: 'invitations/joancinta_back.png'
     },
     'joanmarc': {
-        front: 'invitations/joanmarc_front.PNG',
-        back: 'invitations/joanmarc_back.PNG'
+        front: 'invitations/joanmarc_front.png',
+        back: 'invitations/joanmarc_back.png'
     },
     'jordi': {
-        front: 'invitations/jordi_front.PNG',
-        back: 'invitations/jordi_back.PNG'
+        front: 'invitations/jordi_front.png',
+        back: 'invitations/jordi_back.png'
     },
     'jordig': {
-        front: 'invitations/jordig_front.PNG',
-        back: 'invitations/jordig_back.PNG'
+        front: 'invitations/jordig_front.png',
+        back: 'invitations/jordig_back.png'
     },
     'jordipati': {
-        front: 'invitations/jordipati_front.PNG',
-        back: 'invitations/jordipati_back.PNG'
+        front: 'invitations/jordipati_front.png',
+        back: 'invitations/jordipati_back.png'
     },
     'josepmaria': {
-        front: 'invitations/josepmaria_front.PNG',
-        back: 'invitations/josepmaria_back.PNG'
+        front: 'invitations/josepmaria_front.png',
+        back: 'invitations/josepmaria_back.png'
     },
     'josepmerce': {
-        front: 'invitations/josepmerce_front.PNG',
-        back: 'invitations/josepmerce_back.PNG'
+        front: 'invitations/josepmerce_front.png',
+        back: 'invitations/josepmerce_back.png'
     },
     'juditdani': {  
-        front: 'invitations/juditdani_front.PNG',
-        back: 'invitations/juditdani_back.PNG'
+        front: 'invitations/juditdani_front.png',
+        back: 'invitations/juditdani_back.png'
     },
     'julia': {
-        front: 'invitations/julia_front.PNG',
-        back: 'invitations/julia_back.PNG'
+        front: 'invitations/julia_front.png',
+        back: 'invitations/julia_back.png'
     },
     'lucia': {
-        front: 'invitations/lucia_front.PNG',
-        back: 'invitations/lucia_back.PNG'
+        front: 'invitations/lucia_front.png',
+        back: 'invitations/lucia_back.png'
     },
     'marcelena': {
-        front: 'invitations/marcelena_front.PNG',
-        back: 'invitations/marcelena_back.PNG'
+        front: 'invitations/marcelena_front.png',
+        back: 'invitations/marcelena_back.png'
     },
     'marcerika': {
-        front: 'invitations/marcerika_front.PNG',
-        back: 'invitations/marcerika_back.PNG'
+        front: 'invitations/marcerika_front.png',
+        back: 'invitations/marcerika_back.png'
     },
     'mariajaime': {
-        front: 'invitations/mariajaime_front.PNG',
-        back: 'invitations/mariajaime_back.PNG'
+        front: 'invitations/mariajaime_front.png',
+        back: 'invitations/mariajaime_back.png'
     },
     'marina': {
-        front: 'invitations/marina_front.PNG',
-        back: 'invitations/marina_back.PNG'
+        front: 'invitations/marina_front.png',
+        back: 'invitations/marina_back.png'
     },
     'marinamar': {
-        front: 'invitations/marinamar_front.PNG',
-        back: 'invitations/marinamar_back.PNG'
+        front: 'invitations/marinamar_front.png',
+        back: 'invitations/marinamar_back.png'
     },
     'marisajuanito': {
-        front: 'invitations/marisajuanito_front.PNG',
-        back: 'invitations/marisajuanito_back.PNG'
+        front: 'invitations/marisajuanito_front.png',
+        back: 'invitations/marisajuanito_back.png'
     },
     'marisinagusti': {
-        front: 'invitations/marisinagusti_front.PNG',
-        back: 'invitations/marisinagusti_back.PNG'
+        front: 'invitations/marisinagusti_front.png',
+        back: 'invitations/marisinagusti_back.png'
     },
     'martajordi': {
-        front: 'invitations/martajordi_front.PNG',
-        back: 'invitations/martajordi_back.PNG'
+        front: 'invitations/martajordi_front.png',
+        back: 'invitations/martajordi_back.png'
     },
     'merce': {
-        front: 'invitations/merce_front.PNG',
-        back: 'invitations/merce_back.PNG'
+        front: 'invitations/merce_front.png',
+        back: 'invitations/merce_back.png'
     },
     'meritxell': {
-        front: 'invitations/meritxell_front.PNG',
-        back: 'invitations/meritxell_back.PNG'
+        front: 'invitations/meritxell_front.png',
+        back: 'invitations/meritxell_back.png'
     },
     'miqueljulia': {
-        front: 'invitations/miqueljulia_front.PNG',
-        back: 'invitations/miqueljulia_back.PNG'
+        front: 'invitations/miqueljulia_front.png',
+        back: 'invitations/miqueljulia_back.png'
     },
     'mireia': {
-        front: 'invitations/mireia_front.PNG',
-        back: 'invitations/mireia_back.PNG'
+        front: 'invitations/mireia_front.png',
+        back: 'invitations/mireia_back.png'
     },
     'miriam': {
-        front: 'invitations/miriam_front.PNG',
-        back: 'invitations/miriam_back.PNG'
+        front: 'invitations/miriam_front.png',
+        back: 'invitations/miriam_back.png'
     },
     'nandomontse': {
-        front: 'invitations/nandomontse_front.PNG',
-        back: 'invitations/nandomontse_back.PNG'
+        front: 'invitations/nandomontse_front.png',
+        back: 'invitations/nandomontse_back.png'
     },
     'natalia': {
-        front: 'invitations/natalia_front.PNG',
-        back: 'invitations/natalia_back.PNG'
+        front: 'invitations/natalia_front.png',
+        back: 'invitations/natalia_back.png'
     },
     'polalba': {
-        front: 'invitations/polalba_front.PNG',
-        back: 'invitations/polalba_back.PNG'
+        front: 'invitations/polalba_front.png',
+        back: 'invitations/polalba_back.png'
     },
     'roger': {
-        front: 'invitations/roger_front.PNG',
-        back: 'invitations/roger_back.PNG'
+        front: 'invitations/roger_front.png',
+        back: 'invitations/roger_back.png'
     },
     'rogerhiba': {
-        front: 'invitations/rogerhiba_front.PNG',
-        back: 'invitations/rogerhiba_back.PNG'
+        front: 'invitations/rogerhiba_front.png',
+        back: 'invitations/rogerhiba_back.png'
     },
     'rosa': {
-        front: 'invitations/rosa_front.PNG',
-        back: 'invitations/rosa_back.PNG'
+        front: 'invitations/rosa_front.png',
+        back: 'invitations/rosa_back.png'
     },
     'santi': {
-        front: 'invitations/santi_front.PNG',
-        back: 'invitations/santi_back.PNG'
+        front: 'invitations/santi_front.png',
+        back: 'invitations/santi_back.png'
     },
     'sandra': {
-        front: 'invitations/sandra_front.PNG',
-        back: 'invitations/sandra_back.PNG'
+        front: 'invitations/sandra_front.png',
+        back: 'invitations/sandra_back.png'
     },
     'sergisara': {
-        front: 'invitations/sergisara_front.PNG',
-        back: 'invitations/sergisara_back.PNG'
+        front: 'invitations/sergisara_front.png',
+        back: 'invitations/sergisara_back.png'
     },
     'sergi': {
-        front: 'invitations/sergi_front.PNG',
-        back: 'invitations/sergi_back.PNG'
+        front: 'invitations/sergi_front.png',
+        back: 'invitations/sergi_back.png'
     },
     'xavieranabel': {
-        front: 'invitations/xavieranabel_front.PNG',
-        back: 'invitations/xavieranabel_back.PNG'
+        front: 'invitations/xavieranabel_front.png',
+        back: 'invitations/xavieranabel_back.png'
     }
 }; 
